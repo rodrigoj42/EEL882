ff7d0345f174b82f1b380510aaf88a0448a14908
rodrigoj42 <rodrigoj42@gmail.com>
2019-06-16 22:48:56 -0300

    fix rotation order

--- cg/2/js/ArcballControl.js
+++ cg/2/js/ArcballControl.js
@@ -86,37 +86,6 @@ THREE.ArcballControls = function (_encompassingArcball, _camera, _domElement, _s
       _mouse.y = - ( ( event.clientY - rect.top ) / rect.height ) * 2 + 1;
       _raycaster.setFromCamera( _mouse, _camera );
 
-      let _selected = _intersection.object;
-      if (_selected) {
-        let newIntersection = new THREE.Vector3();
-        if (_selected.name === 'Arcball') {
-          let intersects = _raycaster.intersectObject(_selected)
-          if (intersects.length > 0) {
-            newIntersection = intersects[0].point;
-          }
-        } else {
-          _raycaster.ray.intersectPlane(_plane, newIntersection);
-        }
-        if (newIntersection.z) {
-          let rotationAxis = new THREE.Vector3().crossVectors(newIntersection, _intersection.point).normalize()
-          let angle = newIntersection.angleTo(_intersection.point);
-
-          if (rotationAxis && angle) {
-            angle *= 360 / (2 * Math.PI);
-            let quaternion = new THREE.Quaternion().setFromAxisAngle(rotationAxis, angle);
-            if (_selected.name === 'Arcball') {
-              _selected.applyQuaternion(quaternion);
-            } else {
-              _inverseQuaternion.copy(_encompassingArcball.quaternion).inverse()
-              _selected.parent.applyQuaternion(_inverseQuaternion);
-              _selected.parent.applyQuaternion(quaternion);
-              _selected.parent.applyQuaternion(_encompassingArcball.quaternion);
-            }
-            _intersection.point = newIntersection;
-          }
-        }
-      }
-
       let visibleArcballs = _encompassingArcball.children
         .flatMap((boxes) => boxes.children)
         .filter((arcball) => arcball.material.visible);
@@ -132,6 +101,36 @@ THREE.ArcballControls = function (_encompassingArcball, _camera, _domElement, _s
             _camera.getWorldDirection(_plane.normal),
             _worldPosition.setFromMatrixPosition(object.matrixWorld) 
           );
+        }
+      }
+
+      let _selected = _intersection.object;
+      if (_selected) {
+        let newIntersection = new THREE.Vector3();
+        if (_selected.name === 'Arcball') {
+          let intersects = _raycaster.intersectObject(_selected)
+          if (intersects.length > 0) {
+            newIntersection = intersects[0].point;
+          }
+        } else {
+          _raycaster.ray.intersectPlane(_plane, newIntersection);
+        }
+
+        let rotationAxis = new THREE.Vector3().crossVectors(newIntersection, _intersection.point).normalize()
+        let angle = newIntersection.angleTo(_intersection.point);
+
+        if (rotationAxis && angle) {
+          angle *= 360 / (2 * Math.PI);
+          let quaternion = new THREE.Quaternion().setFromAxisAngle(rotationAxis, angle);
+          if (_selected.name === 'Arcball') {
+            _selected.applyQuaternion(quaternion);
+          } else {
+            _inverseQuaternion.copy(_encompassingArcball.quaternion).inverse()
+            _selected.parent.applyQuaternion(_encompassingArcball.quaternion);
+            _selected.parent.applyQuaternion(quaternion);
+            _selected.parent.applyQuaternion(_inverseQuaternion);
+          }
+          _intersection.point = newIntersection
         }
       }
     }
